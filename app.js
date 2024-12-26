e1fbf64225f2f6009ad55b549bf023ec2fbd013f
S0URABH_choudhary <subh7898765@gmail.com>
2024-12-26 13:35:08 +0530

    completed

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+// image scale
+const cardsimage = document.querySelectorAll('.image');
+
+//  an Intersection Observer
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+      } else {
+        entry.target.classList.remove('active');
+      }
+    });
+  },
+  {
+    root: document.querySelector('.scroll-container'), 
+    threshold: 0.9, // Trigger when 90% of the card is visible
+  }
+);
+
+// Observe each card
+cardsimage.forEach((card) => observer.observe(card));
+
+
+// loader 
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    setTimeout(() => {
+        loader.style.opacity = '0'; 
+        loader.style.pointerEvents = 'none'; 
+        setTimeout(() => {
+            loader.style.display = 'none'; 
+        }, 500); 
+    }, 6000); 
+});
+
+// scroll
+
 document.addEventListener("DOMContentLoaded", () => {
     const scroller = document.querySelector(".scroller");
     const nextButton = document.getElementById("next");
